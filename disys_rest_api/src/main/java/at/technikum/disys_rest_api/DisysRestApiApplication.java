ff7ab14fcc8f210fc901a334c2ac6b852339c6b2
Michael Stermann <wi24b048@technikum-wien.at>
2026-04-15 15:49:55 +0200

    Add REST API project

---
+++ disys_rest_api/src/main/java/at/technikum/disys_rest_api/DisysRestApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.disys_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisysRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisysRestApiApplication.class, args);
+    }
+
+}
---
+++ disys_rest_api/src/test/java/at/technikum/disys_rest_api/DisysRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.disys_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisysRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
