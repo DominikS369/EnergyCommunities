15c899beee29e203566d9633ca681f64d2432b31
condr <condricluka14@gmail.com>
2026-04-21 15:38:35 +0200

    Reset the repository

--- disys_javaFX/src/main/java/disys/project/disys_javafx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package disys.project.disys_javafx;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
